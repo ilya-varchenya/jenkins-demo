128536324af5cbf459428e25248e5306e26b3105
ilya-varchenya <ilya.varchenya@gmail.com>
2024-12-28 07:34:15 +0200

    remove scripts

--- scripts/Tag.groovy
+++
@@ -1,5 +0,0 @@
-if (!branch_name) return ["Please, choose a branch"]
-def proc = "git ls-remote --tags https://github.com/ilya-varchenya/jenkins-demo".execute()
-proc.waitFor()
-def tags = proc.in.text.readLines().findAll { it.contains(branch_name) }.collect { it.split()[1].replace('refs/tags/', '') }
-return tags.isEmpty() ? ["No tags for chosen branch"] : tags
